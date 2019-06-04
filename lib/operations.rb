781a65b0d3ba4e72de58d4a2f28246907a5e21b2
Phil Roth <phil.roth077@gmail.com>
2019-06-04 21:50:14 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,19 @@
 def unsafe?(speed)
+  if speed > 60
+    return true
+  elsif speed < 40
+    return true
+  else
+    return false
+  end
+#  binding.pry
 
 end
 
 
 
 def not_safe?(speed)
-	
+  speed > 60 ? true : speed < 40 ? true : false
+
+
 end
-	
-
-
